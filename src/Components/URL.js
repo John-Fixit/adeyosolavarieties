bfc47ab4bc6b1230af75e0cd8ed259bf1a3f6ff2
Adeoye John Oluwakayode <Johnfixit293@gmail.com>
2023-02-07 22:57:40 +0100

    updated

--- src/Components/URL.js
+++ src/Components/URL.js
@@ -1,3 +1,2 @@
-// export const baseUrl = "https://adeyosolavarieties-backend.vercel.app"
-// export const baseUrl = "https://adeyosolavarieties-9h5iwfqi0-john-fixit.vercel.app"
-export const baseUrl = "http://localhost:4000"
+export const baseUrl = "https://adeyosolavarieties.vercel.app"
+// export const baseUrl = "http://localhost:4000"
